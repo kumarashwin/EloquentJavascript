94d359f0ff9fc726f42502c61808e1198f0e0f00
Ash Kumar <kumar.ashwin@outlook.com>
2016-08-15 14:26:36 -0400

    Added 'Tiger'

--- ElectronicLife/ElectronicLife.js
+++ ElectronicLife/ElectronicLife.js
@@ -218,6 +218,30 @@ SmartPlantEater.prototype.act = function(view){
 };
 // -----------------------
 
+// --- Tiger ---
+function Tiger(){
+    this.energy = 120;
+}
+
+Tiger.prototype.act = function(view){
+    
+    var space = view.find(" ");
+    if (this.energy > 600 && space){
+        return {type: "reproduce", direction: space};
+    }
+
+    var critter = view.find("O");
+    if(critter){
+        return {type:"eat", direction: critter};
+    }
+
+    if(space){
+        return {type:"move", direction: space};
+    }
+
+};
+// -------------
+
 //----- Elements/Characters ----
 function elementFromCharacter(legend, character){
     if (character == " "){

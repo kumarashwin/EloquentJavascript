1a97c00c49ce72e9e98176d0ce06d6516a856cf2
Ash Kumar <kumar.ashwin@outlook.com>
2016-09-14 18:38:55 -0400

    Finished setting up the rendering part, as well as styles

--- PlatformGame/platformGame.js
+++ PlatformGame/platformGame.js
@@ -110,3 +110,93 @@ function Coin(pos){
 }
 
 Coin.prototype.type = "coin";
+
+//ELEMENT
+function element(name, className){
+    var element = document.createElement(name);
+    if(className){
+        element.className = className;
+    }
+    return element;
+}
+
+//DISPLAY
+function DOMDisplay(parent, level){
+    this.wrap = parent.appendChild(element("div", "game"));
+    this.level = level;
+
+    this.wrap.appendChild(this.drawBackground());
+    this.actorLayer = null;
+    this.drawFrame();
+}
+
+//SCALE
+var scale = 20;
+
+DOMDisplay.prototype.drawBackground= function(){
+    var table = element("table", "background");
+    table.style.width = this.level.width * scale + "px";
+    this.level.grid.forEach(function(row){
+        var rowElement = table.appendChild(element("tr"));
+        rowElement.style.height = scale + "px";
+        row.forEach(function(type){
+            rowElement.appendChild(element("td", type));
+        });
+    });
+    return table;
+};
+
+DOMDisplay.prototype.drawActors = function(){
+    var wrap = element("div");
+    this.level.actors.forEach(function(actor){
+        var rect = wrap.appendChild(element("div", "actor " + actor.type));
+
+        rect.style.width = (actor.size.x * scale) + "px";
+        rect.style.height = (actor.size.y * scale) + "px";
+        rect.style.left = (actor.pos.x * scale) + "px";
+        rect.style.top = (actor.pos.y * scale) + "px";
+    });
+    return wrap;
+};
+
+DOMDisplay.prototype.drawFrame = function(){
+    if(this.actorLayer){
+        this.wrap.removeChild(this.actorLayer);
+    }
+    this.actorLayer = this.wrap.appendChild(this.drawActors());
+    this.wrap.className = "game " + (this.level.status || "");
+    this.scrollPlayerIntoView();
+};
+
+DOMDisplay.prototype.scrollPlayerIntoView = function(){
+    var width = this.wrap.clientWidth;
+    var height = this.wrap.clientHeight;
+    var margin = width/3;
+
+    var left = this.wrap.scrollLeft;
+    var right = left + width;
+
+    var top = this.wrap.scrollTop;
+    var bottom = top + height;
+
+    var player = this.level.player;
+    var center = player.pos.plus(player.size.times(0.5)).times(scale);
+
+    if(center.x < left + margin){
+        this.wrap.scrollLeft = center.x - margin;
+    }
+    else if(center.x > right - margin){
+        this.wrap.scrollLeft = center.x + margin - width;
+    }
+
+    if(center.y < top + margin){
+        this.wrap.scrollTop = center.y - margin;
+    }
+    else if(center.y > bottom - margin){
+        this.wrap.scrollTop = center.y + margin - height;
+    }
+};
+
+DOMDisplay.prototype.clear = function(){
+    this.wrap.parentNode.removeChild(this.wrap);
+};

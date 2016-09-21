96d7c57cedc25221b8b4d9f11c2a267232e804b2
Ash Kumar <kumar.ashwin@outlook.com>
2016-09-21 19:21:26 -0400

    The pieChart is now 'Classified'. Get it? 'Class'-ified? Hahaha... yeah. Animation bug, though.

--- Canvas/pieChart/pieChart.js
+++ Canvas/pieChart/pieChart.js
@@ -1,89 +1,109 @@
+
 //====== PIE CHART =====================
-function drawPieChart(context, results, radius, centerX, centerY, stroke, strokeWidth) {
-    var angles = [];
-    var doneAnimation = false;
-    var dumpInterval = false;
+function PieChart(context, results, radius, centerX, centerY, stroke, strokeWidth) {
+    this.context = context;
+    this.data = results;
+    this.slices = [];
+    this.doneAnimation = false;
+    this.dumpInterval = false;
+    this.posX = centerX;
+    this.posY = centerY;
+    this.totalObservations = this.data.reduce(function (sum, choice) { return sum + choice.count; }, 0);
 
-    //AWW YEAH, using closures!
-    function clearFactory(radius, centerX, centerY, stroke, strokeWidth) {
-        radius *= 1.1; //Some padding to be sure;
-        return function () {
-            context.clearRect(centerX - radius, centerY - radius, centerX + radius, centerY + radius);
+    this.size = radius;
+    if(stroke === true){                    // Determine if style is hollow i.e. we use strokes
+        this.stroke = stroke;               // If strokeWidth provided
+        if(strokeWidth) {
+            this.strokeWidth = strokeWidth; // If yes;
+            this.size = radius - ( Math.max(strokeWidth, 40) / 2); // Reduce size by outer half of stroke
+        } else {
+            this.strokeWidth = 40; // If not;
+            this.size = radius - 20; // Reduce size by half of minimum stroke width i.e. 40 / 2
         }
-    };
-    var clear = clearFactory(radius, centerX, centerY, stroke, strokeWidth);
-
-    if (stroke === true && strokeWidth) {
-        strokeWidth = Math.max(strokeWidth, 40);
-        radius = radius - (strokeWidth / 2);
     }
 
-    //First element starts at 90 degrees
-    var startAngle = (-0.5 * Math.PI);
-    var endAngle;
-    var total = results.reduce(function (sum, choice) { return sum + choice.count; }, 0);
+    this.clear = this.clearFactory(this.size, this.posX, this.posY);
+    this.populateSlices();
+    this.draw(this);
+}
 
-    for (var i = 0; i < results.length; i++) {
-        var sliceAngle = (results[i].count / total) * 2 * Math.PI;
+//AWW YEAH, using closures!
+PieChart.prototype.clearFactory = function(radius, centerX, centerY){
+    radius *= 1.1; //Some padding to be sure;
+    return function(){
+        this.context.clearRect(centerX - radius, centerY - radius, centerX + radius, centerY + radius);
+    }
+};
+
+PieChart.prototype.populateSlices = function(){
+    var startAngle = -0.5 * Math.PI; //First element starts at 90 degrees
+    var endAngle;
+    for (var i = 0; i < this.data.length; i++) {
+        var slice = this.data[i];
+        var sliceAngle = (slice.count / this.totalObservations) * 2 * Math.PI; // Percentage of a full circle
         endAngle = startAngle + sliceAngle;
-        angles.push({ startAngle: startAngle, endAngle: endAngle });
+        this.slices.push(new Slice(slice.color, startAngle, endAngle));
         startAngle = endAngle;
     }
+};
 
-    angles.forEach(function (result, index) {
-        var color = results[index].color;
-        var difference = result.endAngle - result.startAngle;
-        var step = Math.max(0.05, difference * 0.05);
-        var startAngle = result.startAngle - (30 * (Math.PI / 180));
-        var endAngle = startAngle;
+PieChart.prototype.paint = function(color){
+    if(this.stroke === true){
+        this.context.lineWidth = this.strokeWidth;
+        this.context.strokeStyle = color;
+        this.context.stroke();
+    } else {
+        context.lineTo(0, 0);
+        context.fillStyle = color;
+        context.fill();
+    }
+};
 
-        var interval = setInterval(function () {
-            if (index == 0) {
-                //In a new chain, if all elements are done animating
-                if (doneAnimation)
-                    //Initiate dump of the interval
-                    dumpInterval = true;
-                else
-                    clear();
-            }
+function Slice(color, beginning, end){
+    this.color = color;
+    this.beginning = beginning;
+    this.end = end;
 
-            if (dumpInterval) {
-                clearInterval(interval);
+    //For step-by-step animation:
+    this.startAngle = this.endAngle = this.beginning - (2*Math.PI/12); // beginning - 30 degrees
+    this.step = Math.max(0.05, (this.beginning - this.end) * 0.05); // Minimum 0.05 upto 5% of slice's angle
+}
+
+PieChart.prototype.draw = function(_this){
+    _this.slices.forEach(function(slice, index){
+        var interval = setInterval(function(){
+            if (index == 0 && _this.doneAnimation) // In a new chain, if all elements are done animating
+                _this.dumpInterval = true;         // initiate dump of the interval
+            else
+                _this.clear()                      // Otherwise, clear the canvas
+
+            if (_this.dumpInterval) {
+                clearInterval(interval); // End animation for the slices
             }
             else {
-                context.save();
-                context.translate(centerX, centerY);
-                context.beginPath();
-                context.arc(0, 0, radius, Math.min(startAngle, result.startAngle), Math.min(endAngle, result.endAngle));
+                slice.startAngle = Math.min(slice.startAngle, slice.beginning);
+                slice.endAngle = Math.min(slice.endAngle, slice.end);
 
-                if (stroke === true) {
-                    context.strokeStyle = color;
-                    context.lineWidth = strokeWidth;
-                    context.stroke();
-                } else {
-                    context.lineTo(0, 0);
-                    context.fillStyle = color;
-                    context.fill();
-                }
+                _this.context.save();
+                _this.context.translate(_this.posX, _this.posY);
+                _this.context.beginPath();
+                _this.context.arc(0, 0, _this.size, slice.startAngle, slice.endAngle);
+                _this.paint(slice.color);
+                _this.context.restore();
 
-                context.restore();
-
-                if (endAngle >= result.endAngle
-                    && startAngle >= result.startAngle) {
-                    //First element start the verification chain
-                    if (index == 0)
-                        doneAnimation = true;
+                if (slice.endAngle == slice.end && slice.startAngle == slice.beginning){ // If animation complete,
+                    if (index == 0) // First element starts the verification chain
+                        _this.doneAnimation = true;
                     else
-                        //All other following elements continue the verification
-                        doneAnimation = doneAnimation && true;
+                        _this.doneAnimation = _this.doneAnimation && true; //All other following elements continue the verification
                 }
-                else {
-                    doneAnimation = false;
-                    startAngle += step / 2;
-                    endAngle += step;
+                else {                                      
+                    _this.doneAnimation = false; // Otherwise, if one slice hasn't finished, 
+                    slice.startAngle += slice.step / 2;     // it sets the doneAnimation to 'false'
+                    slice.endAngle += slice.step;           // and increases the relevant variables
                 }
             }
         }, 30);
     });
-}
+};
 //========== END OF PIE DRAWING PART ========
